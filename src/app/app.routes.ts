9720df44c26dff8c6ec6ee32b40546b1cd8b7083
guavaandnobi <guavaandnobi@instai.co>
2024-07-03 17:07:14 +0000

    [fev2][dev] Add route to model-example-form
    Add route.
    
    Bug: None
    Test: None

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { ModelExamplePageComponent } from './model-example-page/model-example-page.component';
 import { authGuard } from './auth/auth.guard';
+import { ModelExampleFormComponent } from './model-example-form/model-example-form.component';
 
 export const routes: Routes = [
   {
     path: 'model-examples',
     component: ModelExamplePageComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'model-examples/:model',
+    component: ModelExampleFormComponent,
     canActivate: [authGuard],
   },
 ];
--- src/app/model-example-page/model-example-page.component.ts
+++ src/app/model-example-page/model-example-page.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+class Example {
+  title: string = '';
+  description: string = '';
+  model: string = '';
+}
 
 @Component({
   selector: 'app-model-example-page',
   standalone: true,
-  imports: [NgOptimizedImage, RouterModule],
+  imports: [CommonModule, NgOptimizedImage, RouterModule],
   templateUrl: './model-example-page.component.html',
   styleUrl: './model-example-page.component.scss',
 })
-export class ModelExamplePageComponent {}
+export class ModelExamplePageComponent {
+  examples: Example[] = [
+    {
+      title: 'Yolo V8 Testing',
+      description:
+        'This is yolo8 testing example for private usage. ' +
+        'Click and follow the instruction to run the example.' +
+        'abc',
+      model: 'yolov8-testing',
+    },
+    {
+      title: 'Yolo V8 Training',
+      description:
+        'This is yolo8 training example for private usage. ' +
+        'Click and follow the instruction to run the example.' +
+        'abc',
+      model: 'yolov8-training',
+    },
+    {
+      title: 'Yolo V8 Detecting',
+      description:
+        'This is yolo8 dectecting example for private usage. ' +
+        'Click and follow the instruction to run the example.' +
+        'abc',
+      model: 'yolov8-detecting',
+    },
+    {
+      title: 'Yolo3 Tiny Training',
+      description:
+        'This is yolo3 tiny training example for private usage. ' +
+        'Click and follow the instruction to run the example.' +
+        'abc',
+      model: 'yolo3-tiny-training',
+    },
+    {
+      title: 'Yolo3 Tiny Testing',
+      description:
+        'This is yolo3 tiny testing example for private usage. ' +
+        'Click and follow the instruction to run the example.' +
+        'abc',
+      model: 'yolo3-tiny-testing',
+    },
+  ];
+}
