30179491d5c27cd48ad701bb6ae84df7790cee43
Ubuntu <ubuntu@ip-172-31-17-18.ec2.internal>
2024-07-03 10:46:49 +0000

    [fev2][dev] Add signin component
    Bug: None
    Test: None

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.scss'
+  styleUrl: './signin.component.scss',
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
+  disabled: boolean = false;
+  inprogress: boolean = false;
+  username: string = '';
+  signInForm = this.formBuilder.group({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  constructor(private service: AuthService, private formBuilder: FormBuilder) {}
 
+  ngOnInit() {
+    this.username = this.service.getUsername();
+    this.disabled = this.service.getValid();
+    this.service.validObserver.subscribe((value) => {
+      this.inprogress = false;
+      this.disabled = value;
+      this.username = this.service.getUsername();
+    });
+  }
+
+  onSubmit() {
+    this.inprogress = true;
+    this.service.signIn(
+      this.signInForm.value.username!,
+      this.signInForm.value.email!,
+      this.signInForm.value.password!
+    );
+  }
+
+  signOut() {
+    this.service.signOut();
+  }
 }
