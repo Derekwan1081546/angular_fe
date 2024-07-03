de8f0ed202352b7404879d06400faadf28771da9
guavaandnobi <guavaandnobi@instai.co>
2024-07-03 17:07:14 +0000

    [fev2][dev] Add route to model-example-form
    Add route.
    
    Bug: None
    Test: None

--- src/app/model-example-form/model-example-form.component.ts
+++ src/app/model-example-form/model-example-form.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+class SupportedModel {
+  model: string = '';
+  uploadUrl: string = '';
+
+  constructor(model: string, uploadUrl: string) {
+    this.model = model;
+    this.uploadUrl = uploadUrl;
+  }
+};
 
 @Component({
   selector: 'app-model-example-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './model-example-form.component.html',
-  styleUrl: './model-example-form.component.scss'
+  styleUrl: './model-example-form.component.scss',
 })
-export class ModelExampleFormComponent {
+export class ModelExampleFormComponent implements OnInit {
+  uploadForm: FormGroup = new FormGroup({
+    model: new FormControl(''),
+  });
+  private model: string = '';
 
+  supportedModels = [
+    new SupportedModel('yolov8-testing', 'de')
+  ];
+
+  constructor(private readonly route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.model = this.route.snapshot.paramMap.get('model')!;
+    this.uploadForm.get('model')!.setValue(this.model);
+  }
 }
